137d118220fa7dd1e12c261b692fc89707f3ab05
Hatakee Fira <fisan.onodaconan@gmail.com>
2024-05-15 22:11:01 +0700

    Develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Todos from "./components/Todos";
+import TodoForm from "./components/TodoForm";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 // import "./App.css";
@@ -23,8 +24,6 @@ function App() {
     },
   ]);
 
-  console.log(todos);
-
   const toggleCompleted = (todoId) => {
     const updatedTodos = todos.map((todo) => {
       if (todo.id === todoId) {
@@ -40,12 +39,28 @@ function App() {
     setTodos(result);
   };
 
+  const addTodo = (todoTitle) => {
+    if (todoTitle === "") {
+      return;
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    };
+
+    const updatedTodos = todos.concat(newTodo);
+    setTodos(updatedTodos);
+  };
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
       {/* {todos.map((todo) => {
         return <p key={todo.id}>{todo.title}</p>;
       })} */}
+      <TodoForm addTodo={addTodo} />
       <Todos
         todos={todos}
         toggleCompleted={toggleCompleted}

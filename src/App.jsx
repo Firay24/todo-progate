71fac0eefd43a6b255ac6218836c74e133bf3a1c
Hatakee Fira <fisan.onodaconan@gmail.com>
2024-05-15 21:48:46 +0700

    Develop toggle todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,23 @@ function App() {
 
   console.log(todos);
 
+  const toggleCompleted = (todoId) => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
       {/* {todos.map((todo) => {
         return <p key={todo.id}>{todo.title}</p>;
       })} */}
-      <Todos todos={todos} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} />
     </div>
   );
 }
